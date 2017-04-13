9567effad9097856e939171832841414380a588a
Jerry_Z <970636183@qq.com>
2017-04-13 21:48:38 +0800

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,41 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+//获取图片相关的数据
+var imageDatas=require('../data/imageDatas.json');
+// let yeomanImage = require('../images/yeoman.png');
+
+//利用自执行函数，将图片名信息转成图片ＵＲＬ路径信息
+imageDatas=(function getImageURL(imageDatasArr){
+	for(var i=0,j=imageDatasArr.length;i<j;i++){
+		var singleImageData=imageDatasArr[i];
+		singleImageData.imageURL=require('../images/'+singleImageData.fileName);
+		imageDatasArr[i]=singleImageData;
+	}
+	 return imageDatasArr;
+
+})(imageDtas);
+ // imageDatas=getImageURL(imageDatas)
+
+class AppComponent extends React.Component {
+  render：function() {
+    return (
+     	
+     		<section className="stage">
+     			<section className="img-sec" >
+
+     			</section>
+	     		<nav className="controller-nav">
+	     		</nav>
+     		</section>
+  	
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;

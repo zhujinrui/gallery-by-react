5e6137dd746540514306c02265bf6145fa13ad1f
Jerry_Z <970636183@qq.com>
2017-04-14 09:53:13 +0800

    third push

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 //获取图片相关的数据
 var imageDatas=require('../data/imageDatas.json');
-// let yeomanImage = require('../images/yeoman.png');
+
 
 //利用自执行函数，将图片名信息转成图片ＵＲＬ路径信息
 imageDatas=(function getImageURL(imageDatasArr){
@@ -16,14 +16,14 @@ imageDatas=(function getImageURL(imageDatasArr){
 	}
 	 return imageDatasArr;
 
-})(imageDtas);
+})(imageDatas);
+
  // imageDatas=getImageURL(imageDatas)
 
 class AppComponent extends React.Component {
-  render：function() {
+  render(){
     return (
-     	
-     		<section className="stage">
+     	    <section className="stage">
      			<section className="img-sec" >
 
      			</section>
